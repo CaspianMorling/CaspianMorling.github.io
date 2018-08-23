2f9e97ca50b2d82fa5735ab43d2b294264735df0
Caspian <kapian.morling@gmail.com>
2018-08-23 19:40:08 +0200

    test .1

--- app.jsx
+++ app.jsx
@@ -1,9 +1,24 @@
+class Block extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            reaveled: false,
+            adjacent: 0,
+            bomb: 0,
+        }
+    }
+    render(){
+        return (
+            <h1>Hello React!</h1>
+         );
+    }
+};
 
 class App extends React.Component {
  render() {
     return (
         <div id="app">
-           <p>hello</p>
+            <Block />
         </div>
     );
   }
